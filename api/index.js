a81cffa0db3c01779a0a05cf8209ab25233384d5
rabiahaucha <rabiahaucha9@gmail.com>
2023-08-14 15:55:32 +0200

    yes

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const {express,routes} = require('./controller')
+const path = require('path')
+const app = express()
+const port = +process.env.PORT || 3000
+
+
+//static
+app.use (express.static('../static')
+)
+app.use(express.urlencoded({
+      extended: false
+  }),
+  routes)
+
+routes.get('^/$|/challenger', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, '../static/html/index.html'))
+})
+
+app.listen(port, ()=>{
+    console.log(`You are using port: ${port}`);
+})
